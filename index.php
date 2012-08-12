0fa580dd7a0051c4567928a41bc2f1b4a478de60
bis5 <bis5@bis5.mydns.jp>
2012-08-12 16:48:22 +0900

    update xoops_version.php and index.php (refs none module)

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-	include("hako-main.php");
-?>
+/**
+ * @package hako-xc
+ * @author bis5 <bis5@bis5.mydns.jp>
+ */
+
+include dirname(dirname(__FILE__)) . "/mainfile.php";
+
+$controller =& XCube_Root::getSingleton();
+$controller->mController->executeHeader();
+
+$render =& $controller->mContext->mModule->getRenderTarget();
+$render->setTemplateName('hako-xc' . '_top.tpl');
+
+$controler->mController->executeView();
--- xoops_version.php
+++ xoops_version.php
@@ -3,17 +3,33 @@
 $modversion['name'] = 'hako-xc';
 $modversion['version'] = "1.1";
 $modversion['description'] = 'Hakoniwa clone';
-$modversion['credits'] = "";
-$modversion['author'] = "";
-$modversion['help'] = "";
-$modversion['license'] = "";
-$modversion['official'] = "no";
+$modversion['credits'] = "bis5@bis5.mydns.jp";
+$modversion['author'] = "bis5";
+$modversion['help'] = null;
+$modversion['license'] = "GPL";
+$modversion['official'] = 0;
 $modversion['image'] = "hako_image/hakoniwa.gif";
 $modversion['dirname'] = "hako-xc";
+$modversion['cube_style'] = true;
 // Admin things
 $modversion['hasAdmin'] = 1;
 $modversion['adminindex'] = "hako-mente.php";
 $modversion['adminmenu'] = "";
 
+// DB
+$modversion['sqlfile'] = false;
+$modversion['tables'] = array();
+
 // Menu
 $modversion['hasMain'] = 1;
+
+// Search
+$modversion['hasSearch'] = 0;
+
+// Notification
+$modversion['hasNotification'] = 0;
+
+//comments
+$modversion['hasComments'] = 0;
+
+$modversion['templates'][] = array('file' => 'top.tpl');
